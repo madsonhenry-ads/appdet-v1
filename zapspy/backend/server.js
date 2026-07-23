5a14abd1ae7d07fcca31328464e51f00f11174b6
madsonhenry <madsonhenry.ads@gmail.com>
2026-07-23 22:15:44 +0100

    Fix route order: /digi/lz2 before /digi to prevent capture

--- zapspy/backend/server.js
+++ zapspy/backend/server.js
@@ -229,8 +229,8 @@ app.use('/es', express.static(path.join(funnelPath, 'espanhol')));
 app.use('/pt', express.static(path.join(funnelPath, 'portugues')));
 app.use('/fr', express.static(path.join(funnelPath, 'frances')));
 app.use('/ingles2', express.static(path.join(funnelPath, 'ingles2')));
-app.use('/digi', express.static(path.join(funnelPath, 'digi', 'ingles2')));
 app.use('/digi/lz2', express.static(path.join(funnelPath, 'digi', 'lz2')));
+app.use('/digi', express.static(path.join(funnelPath, 'digi', 'ingles2')));
 app.use('/vega', express.static(path.join(__dirname, '..', 'vega', 'ingles2')));
 
 // Domain-based routing
